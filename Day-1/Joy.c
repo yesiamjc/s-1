5f9fba42dd58f54a8d7af7721bd80cb40aed1c8f
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-03-19 08:39:41 +0530

    Day -1

---
+++ Day-1/Joy.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+int main()
+{
+    printf("Joy Chakraborty");
+    return 0;
+}
---
+++ Day-1/var.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int var=3;
+    printf("%d \n",var);
+    int var1,var2,var3;
+    var1=var2=var3=69;
+    printf("%d \n",var1);
+    printf("%d \n",var2);
+    printf("%d \n",var3);
+}
