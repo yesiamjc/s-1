f77298aaad06e85f678fa486d84f88ffcd14723d
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-04-06 08:08:35 +0530

    Day 1 Code Upload

---
+++ Day1/placeholde.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int a=7;
+    printf("Output a as : %d",a);
+    return 0;
+}
---
+++ Day1/variable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int var1=3,var2,var3;
+    var2=4;
+    var3=5;
+    printf("Var1=%d\nVar2=%d\nVar3=%d",var1,var2,var3);
+    return 0;
+}
